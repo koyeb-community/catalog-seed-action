92e039b1d45a0211e1c5b48d5a6f2bb8d50e36a3
Charly Molter <charly@koyeb.com>
2020-11-03 15:56:36 +0100

    Add seed to action

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const core = require('@actions/core');
 const fnSeed = require("./function.js");
+const axios = require("axios");
+
+
+const seed = async (apiUrl, loginPayload, payload) => {
+    const resp = await axios.post(`${apiUrl}/v1/account/login`, loginPayload)
+    const opts = {
+        headers: {
+            "Authorization": `bearer ${resp.data.token.id}`
+        }
+    }
+    try {
+        await axios.post(`${apiUrl}/v1/internal/storage/seed`, payload, opts)
+    } finally {
+        await axios.delete(`${apiUrl}/v1/account/logout`, opts)
+    }
+}
 
 (async () => {
     try {
+        const apiUrl = core.getInput('api_url')
+        const loginPayload = {email: core.getInput('api_username'), password: core.getInput('api_password')}
         const org = core.getInput('organization')
 
         const repoName = process.env.GITHUB_REPOSITORY.split("/")[1]
@@ -12,8 +30,8 @@ const fnSeed = require("./function.js");
             core.info(`${version} is not a valid semver like v1.0.0, skipping seed`)
             return
         }
-        const res = await fnSeed.makeCatalogFunction(".", repoName, repoUrl, org, "v1.0.0")
-        core.info(JSON.stringify(res))
+        const catalogDef = await fnSeed.makeCatalogFunction(".", repoName, repoUrl, org, version)
+        await seed(apiUrl, loginPayload, {"catalog_functions": [catalogDef]})
     } catch (error) {
         core.setFailed(error.message);
     }
